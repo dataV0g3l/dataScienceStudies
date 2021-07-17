1ab23d0416c336c8c7fa2e0147cf7b881d905b17
Vladimir Podobreev <subscriptions.v0g3l@gmail.com>
2021-07-17 17:04:09 +0300

    added singleton project

---
+++ Patterns/Singleton Pattern/Singleton/HealthState.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class HealthState
+    {
+        private static HealthState healthState;
+        private static readonly Object syncLock = new Object();
+
+        private List<string> Errors { get; set; } = new List<string>();
+        private List<int> Results { get; set; } = new List<int>();
+        private int Done { get; set; } = 0;
+        private int Total { get; set; } = 0;
+
+        public void TaskFinished(int res)
+        {
+            lock(syncLock)
+            {
+                Total++;
+                Done++;
+                Results.Add(res);
+            }
+        }
+
+        public void TaskFailed(string msg)
+        {
+            lock (syncLock)
+            {
+                Total++;
+                Errors.Add(msg);
+            }
+        }
+
+        public string GetTextReport()
+        {
+            return $"Executed: {Total}, \r\nTask failed: {Total - Done},\r\nresults: {String.Join(',', Results)}";
+        }
+
+        public static HealthState GetInstance()
+        {
+            if (healthState == null)
+            {
+                lock(syncLock)
+                {
+                    healthState = new HealthState();
+                }
+            }
+
+            return healthState;
+        }
+    }
+}
---
+++ Patterns/Singleton Pattern/Singleton/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Singleton
+{
+    public class Worker
+    {
+        private Random rnd = new Random();
+
+        public int DoWork()
+        {
+            int res = rnd.Next(10);
+
+            Thread.Sleep(res * 1000);
+
+            if (res == 3)
+            {
+                throw new Exception("PANIC!!!");
+            }
+
+            return res;
+        }
+    }
+}
